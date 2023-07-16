0aeee4a284920e59b203fd9891f32b2e6a10b6a6
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-07-16 12:04:35 +0200

    CM-24423 - Implement simple CLI wrapper

--- src/main/kotlin/com/cycode/plugin/services/CycodeService.kt
+++ src/main/kotlin/com/cycode/plugin/services/CycodeService.kt
@@ -1,18 +1,31 @@
 package com.cycode.plugin.services
 
+import com.cycode.plugin.Consts.Companion.CLI_PATH
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.cycode.plugin.CycodeBundle
+import com.cycode.plugin.utils.CliWrapper
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
 
 @Service(Service.Level.PROJECT)
-class CycodeService(project: Project) {
+class CycodeService(val project: Project) {
 
     init {
         thisLogger().info(CycodeBundle.message("projectService", project.name))
     }
 
     fun startAuth() {
-        thisLogger().warn("Start auth...")
+        object : Task.Backgroundable(project, "Start auth...", true) {
+            override fun run(indicator: ProgressIndicator) {
+                println(CliWrapper(CLI_PATH).executeCommand("--output=json", "auth", "check"))
+
+                // TODO: increase timeout
+                println(CliWrapper(CLI_PATH).executeCommand("--output=json", "auth"))
+
+                println(CliWrapper(CLI_PATH).executeCommand("--output=json", "auth", "check"))
+            }
+        }.queue()
     }
 }
