ff65edf7fc05ee374f27ed2ee475fa69f72665ca
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-11-07 16:08:38 +0100

    CM-28912 - Add handling of missing LIBC on Linux

---
+++ src/main/kotlin/com/cycode/plugin/cli/ErrorHandling.kt
@@ -0,0 +1,28 @@
+package com.cycode.plugin.cli
+
+import com.cycode.plugin.CycodeBundle
+
+enum class ErrorCode {
+    MISSING_C_STANDARD_LIBRARY,
+    UNKNOWN
+}
+
+const val MISSING_C_STANDARD_LIBRARY_SEARCH = "GLIBC"
+
+private fun caseInsensitiveSearch(output: String, search: String): Boolean {
+    return output.toLowerCase().contains(search.toLowerCase())
+}
+
+fun detectErrorCode(output: String): ErrorCode {
+    return when {
+        caseInsensitiveSearch(output, MISSING_C_STANDARD_LIBRARY_SEARCH) -> ErrorCode.MISSING_C_STANDARD_LIBRARY
+        else -> ErrorCode.UNKNOWN
+    }
+}
+
+fun getUserFriendlyCliErrorMessage(errorCode: ErrorCode): String {
+    return when (errorCode) {
+        ErrorCode.MISSING_C_STANDARD_LIBRARY -> CycodeBundle.message("missingCStandardLibraryErrorNotification")
+        else -> CycodeBundle.message("unknownErrorNotification")
+    }
+}
