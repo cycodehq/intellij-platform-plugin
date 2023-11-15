52ac0eb2432bb5791199182aeb6a26c3e32be043
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-11-15 08:04:19 +0100

    CM-29150 - Add open problems tab action for notification about found detections (#22)

--- src/main/kotlin/com/cycode/plugin/utils/CycodeNotifier.kt
+++ src/main/kotlin/com/cycode/plugin/utils/CycodeNotifier.kt
@@ -4,7 +4,24 @@ import com.cycode.plugin.CycodeBundle
 import com.intellij.notification.NotificationGroup
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindowManager
+
+
+class OpenProblemsTabAction : AnAction(CycodeBundle.message("openProblemsTabAction")) {
+    override fun actionPerformed(e: AnActionEvent) {
+        val manager = e.project?.let { ToolWindowManager.getInstance(it) }
+        if (manager != null) {
+            manager.getToolWindow(PROBLEMS_TOOL_WINDOW_ID)?.activate(null)
+        }
+    }
+
+    companion object {
+        const val PROBLEMS_TOOL_WINDOW_ID = "Problems View"
+    }
+}
 
 
 object CycodeNotifier {
@@ -21,6 +38,13 @@ object CycodeNotifier {
     fun notifyInfo(project: Project?, content: String?) {
         getNotificationGroup()
             .createNotification(CycodeBundle.message("notificationTitle"), content!!, NotificationType.INFORMATION)
+            .notify(project)
+    }
+
+    fun notifyDetections(project: Project?, content: String?) {
+        getNotificationGroup()
+            .createNotification(CycodeBundle.message("notificationTitle"), content!!, NotificationType.INFORMATION)
+            .addAction(OpenProblemsTabAction())
             .notify(project)
     }
 }
