83258a3a15ab09af0a068c84106f9b2a8d4c6b32
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-11-17 16:03:35 +0100

    CM-29254 - Fix code annotation when text offset is not relevant anymore (#24)

--- src/main/kotlin/com/cycode/plugin/services/ScanResultsService.kt
+++ src/main/kotlin/com/cycode/plugin/services/ScanResultsService.kt
@@ -1,15 +1,47 @@
 package com.cycode.plugin.services
 
 import com.cycode.plugin.cli.CliResult
+import com.cycode.plugin.cli.CliScanType
 import com.cycode.plugin.cli.models.scanResult.secret.SecretScanResult
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.util.TextRange
 
 @Service(Service.Level.PROJECT)
 class ScanResultsService {
-    var secretsResults: CliResult<SecretScanResult>? = null
+    private val detectedSegments = mutableMapOf<Pair<CliScanType, TextRange>, String>()
+    private var secretsResults: CliResult<SecretScanResult>? = null
 
     init {
         thisLogger().info("CycodeResultsService init")
+    }
+
+    fun setSecretsResults(result: CliResult<SecretScanResult>) {
+        clearDetectedSegments()
+        secretsResults = result
+    }
+
+    fun getSecretsResults(): CliResult<SecretScanResult>? {
+        return secretsResults
+    }
+
+    fun clear() {
+        secretsResults = null
+    }
+
+    fun hasResults(): Boolean {
+        return secretsResults != null
+    }
+
+    fun saveDetectedSegment(scanType: CliScanType, textRange: TextRange, value: String) {
+        detectedSegments[Pair(scanType, textRange)] = value
+    }
+
+    fun getDetectedSegment(scanType: CliScanType, textRange: TextRange): String? {
+        return detectedSegments[Pair(scanType, textRange)]
+    }
+
+    private fun clearDetectedSegments() {
+        detectedSegments.clear()
     }
 }
