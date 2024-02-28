fffc1622d348c209aecec6c7175f6a1f2acd90ff
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-02-28 14:17:12 +0100

    CM-32755 - Improve loading tab

--- src/main/kotlin/com/cycode/plugin/components/toolWindow/components/loadingContentTab/LoadingContentTab.kt
+++ src/main/kotlin/com/cycode/plugin/components/toolWindow/components/loadingContentTab/LoadingContentTab.kt
@@ -1,28 +1,17 @@
 package com.cycode.plugin.components.toolWindow.components.loadingContentTab
 
+import com.cycode.plugin.CycodeBundle
 import com.cycode.plugin.components.Component
-import com.cycode.plugin.components.common.BorderedPanel
-import com.cycode.plugin.components.common.createClickableLabel
 import com.cycode.plugin.services.CycodeService
-import com.intellij.util.ui.JBUI
+import com.intellij.ui.components.JBLoadingPanel
 import java.awt.BorderLayout
-import java.awt.GridBagConstraints
-import java.awt.GridBagLayout
 import javax.swing.JPanel
 
 class LoadingContentTab : Component<CycodeService>() {
     override fun getContent(service: CycodeService): JPanel {
-        return BorderedPanel().apply {
-            add(JPanel().apply {
-                layout = GridBagLayout()
-                add(add(JPanel().apply {
-                    add(createClickableLabel("Cycode is loading..."))
-                }), GridBagConstraints().apply {
-                    gridy = 0
-                    insets = JBUI.insetsBottom(10)
-                    anchor = GridBagConstraints.NORTHWEST
-                })
-            }, BorderLayout.NORTH)
+        return JBLoadingPanel(BorderLayout(), service).apply {
+            setLoadingText(CycodeBundle.message("loadingTabLoadingText"))
+            startLoading()
         }
     }
 }
