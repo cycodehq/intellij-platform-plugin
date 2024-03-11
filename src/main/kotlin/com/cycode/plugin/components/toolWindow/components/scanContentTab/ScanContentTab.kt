ef76df01d0b4713f210eb2c8578e012054b82abd
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-03-11 11:39:36 +0100

    refactor ScanContentTab

--- src/main/kotlin/com/cycode/plugin/components/toolWindow/components/scanContentTab/ScanContentTab.kt
+++ src/main/kotlin/com/cycode/plugin/components/toolWindow/components/scanContentTab/ScanContentTab.kt
@@ -8,43 +8,37 @@ import com.intellij.util.ui.JBUI
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
 import javax.swing.JButton
+import javax.swing.JComponent
 import javax.swing.JPanel
 
 class ScanContentTab : Component<CycodeService>() {
+    private val panel = JPanel(GridBagLayout())
+    private val gbc = GridBagConstraints().apply {
+        gridy = 0
+        insets = JBUI.insetsBottom(10)
+        anchor = GridBagConstraints.NORTHWEST
+        fill = GridBagConstraints.HORIZONTAL
+    }
+
+    private fun addComponentToPanel(component: JComponent) {
+        gbc.gridy++
+        panel.add(component, gbc)
+    }
+
     override fun getContent(service: CycodeService): JPanel {
-        val gbc = GridBagConstraints().apply {
-            gridy = 0
-            insets = JBUI.insetsBottom(10)
-            anchor = GridBagConstraints.NORTHWEST
-            fill = GridBagConstraints.HORIZONTAL
-        }
-
-        val panel = JPanel(GridBagLayout())
-
-        gbc.gridy++
-        panel.add(createClickableLabel(CycodeBundle.message("scanTabTitleLabel")), gbc)
-
-        gbc.gridy++
-        panel.add(
+        addComponentToPanel(createClickableLabel(CycodeBundle.message("scanTabTitleLabel")))
+        addComponentToPanel(
             JButton(CycodeBundle.message("scanTabSecretsBtn")).apply {
                 addActionListener { service.startSecretScanForCurrentProject() }
             },
-            gbc
         )
-
-        gbc.gridy++
-        panel.add(
+        addComponentToPanel(
             JButton(CycodeBundle.message("scanTabScaBtn")).apply {
                 addActionListener { service.startScaScanForCurrentProject() }
             },
-            gbc
         )
-
-        gbc.gridy++
-        panel.add(createClickableLabel(CycodeBundle.message("scanTabOnSaveTip")), gbc)
-
-        gbc.gridy++
-        panel.add(createClickableLabel(CycodeBundle.message("howToUseLabel")), gbc)
+        addComponentToPanel(createClickableLabel(CycodeBundle.message("scanTabOnSaveTip")))
+        addComponentToPanel(createClickableLabel(CycodeBundle.message("howToUseLabel")))
 
         return panel
     }
