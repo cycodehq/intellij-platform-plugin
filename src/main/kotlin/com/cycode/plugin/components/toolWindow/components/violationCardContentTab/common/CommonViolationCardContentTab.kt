6fd5a21b54859209b04468996b1056bb4fa7ac05
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-04-08 16:30:07 +0200

    fix card centering

--- src/main/kotlin/com/cycode/plugin/components/toolWindow/components/violationCardContentTab/common/CommonViolationCardContentTab.kt
+++ src/main/kotlin/com/cycode/plugin/components/toolWindow/components/violationCardContentTab/common/CommonViolationCardContentTab.kt
@@ -5,6 +5,7 @@ import com.intellij.util.ui.JBUI
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
 import javax.swing.JComponent
+import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.JScrollPane
 
@@ -23,6 +24,10 @@ open class CommonViolationCardContentTab {
             gbc.gridy++
             panel.add(component, gbc)
         }
+
+        // blank JLabel to fill the remaining space otherwise the components are centered
+        gbc.weighty = 1.0
+        panel.add(JLabel(" "), gbc)
 
         // FIXME(MarshalX): something gives left border already. that's why left != right here
         panel.border = JBUI.Borders.empty(10, 2, 0, 10)
