aae8b0b943f0afb7c974e6a8c8f79fd19a2604ab
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-05-15 09:40:04 +0200

    remove unused var

--- src/main/kotlin/com/cycode/plugin/cli/models/scanResult/sast/SastDetection.kt
+++ src/main/kotlin/com/cycode/plugin/cli/models/scanResult/sast/SastDetection.kt
@@ -3,8 +3,6 @@ package com.cycode.plugin.cli.models.scanResult.sast
 import com.cycode.plugin.CycodeBundle
 import com.cycode.plugin.cli.models.scanResult.DetectionBase
 
-const val SAST_MAX_TITLE_LENGTH = 100
-
 data class SastDetection(
     val message: String,
     override val detectionDetails: SastDetectionDetails,
