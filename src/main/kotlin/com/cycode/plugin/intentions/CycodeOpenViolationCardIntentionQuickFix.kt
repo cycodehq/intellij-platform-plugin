a36166da71876ef4554c6534c539cd893749ebc7
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-05-22 16:36:18 +0200

    CM-35960 - Leave "Open violation card" as only one quick fix action (#69)

--- src/main/kotlin/com/cycode/plugin/intentions/CycodeOpenViolationCardIntentionQuickFix.kt
+++ src/main/kotlin/com/cycode/plugin/intentions/CycodeOpenViolationCardIntentionQuickFix.kt
@@ -4,6 +4,7 @@ import com.cycode.plugin.CycodeBundle
 import com.cycode.plugin.cli.models.scanResult.DetectionBase
 import com.cycode.plugin.components.toolWindow.CycodeToolWindowFactory
 import com.cycode.plugin.components.toolWindow.activateToolWindow
+import com.cycode.plugin.icons.PluginIcons
 import com.intellij.codeInsight.intention.PriorityAction
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction
 import com.intellij.openapi.diagnostic.thisLogger
@@ -19,7 +20,22 @@ class CycodeOpenViolationCardIntentionQuickFix(
 ) :
     BaseIntentionAction(), PriorityAction, Iconable {
     override fun getText(): String {
-        return CycodeBundle.message("violationCardIntentionText", detection.getFormattedMessage())
+        var detectionHashCode = detection.hashCode()
+        if (detectionHashCode < 0) detectionHashCode *= -1 // make positive
+
+        val detectionUniqueId = detectionHashCode
+            .toString(16) // convert to hex
+            .slice(0..5) // take the first 6 characters
+
+        var text = detection.getFormattedMessage()
+        // cut too long messages
+        if (text.length > 50) {
+            text = text.substring(0, 50) + "..."
+        }
+
+        return CycodeBundle.message(
+            "violationCardIntentionText", text, detectionUniqueId
+        )
     }
 
     override fun getFamilyName(): String {
@@ -38,15 +54,23 @@ class CycodeOpenViolationCardIntentionQuickFix(
 
         thisLogger().warn("Open violation card quick fix intention has been invoked")
 
-        CycodeToolWindowFactory.TabManager.getTab(project)?.getTreeView()?.displayViolationCard(detection)
+        CycodeToolWindowFactory.TabManager.getTab(project)
+            ?.getTreeView()
+            ?.displayViolationCard(detection)
+
         activateToolWindow(project)
     }
 
     override fun getPriority(): PriorityAction.Priority {
-        return PriorityAction.Priority.NORMAL
+        return when (detection.severity.toLowerCase()) {
+            "critical" -> PriorityAction.Priority.TOP
+            "high" -> PriorityAction.Priority.HIGH
+            "medium" -> PriorityAction.Priority.NORMAL
+            else -> PriorityAction.Priority.LOW
+        }
     }
 
     override fun getIcon(flags: Int): Icon {
-        return com.intellij.icons.AllIcons.Actions.IntentionBulbGrey
+        return PluginIcons.getSeverityIcon(detection.severity)
     }
 }
