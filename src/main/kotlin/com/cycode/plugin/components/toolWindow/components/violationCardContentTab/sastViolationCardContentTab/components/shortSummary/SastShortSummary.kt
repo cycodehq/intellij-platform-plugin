f5f6a8d2c13249fa3925f5669879caaa22e400d8
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-05-22 12:10:31 +0200

    CM-35958 - Make CWE and CVE clickable on violation cards

--- src/main/kotlin/com/cycode/plugin/components/toolWindow/components/violationCardContentTab/sastViolationCardContentTab/components/shortSummary/SastShortSummary.kt
+++ src/main/kotlin/com/cycode/plugin/components/toolWindow/components/violationCardContentTab/sastViolationCardContentTab/components/shortSummary/SastShortSummary.kt
@@ -1,16 +1,19 @@
 package com.cycode.plugin.components.toolWindow.components.violationCardContentTab.sastViolationCardContentTab.components.shortSummary
 
+import com.cycode.plugin.CycodeBundle
 import com.cycode.plugin.cli.models.scanResult.sast.SastDetection
 import com.cycode.plugin.components.toolWindow.components.violationCardContentTab.common.shortSummary.CardShortSummary
+import com.cycode.plugin.components.toolWindow.components.violationCardContentTab.renderCweCveLink
 import javax.swing.JComponent
 
 class SastShortSummary : CardShortSummary() {
     fun getContent(detection: SastDetection): JComponent {
         var shortSummary = detection.severity
 
-        val cwe = detection.detectionDetails.cwe.joinToString(", ")
+        val renderedCwe = detection.detectionDetails.cwe.map { renderCweCveLink(it) }
+        val cwe = renderedCwe.joinToString(", ")
         if (cwe.isNotEmpty()) {
-            shortSummary += " | $cwe"
+            shortSummary = CycodeBundle.message("sastViolationCardShortSummary", shortSummary, cwe)
         }
 
         return getContent(shortSummary)
