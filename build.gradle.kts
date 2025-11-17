d1a5c7396e6fd1a9199f41ad63ece8c7f74abf1d
Elizabeth Braae <elizabeth@cycode.com>
2025-11-17 18:34:09 +0200

    CM-55396: add testing for available 2025.3 IDE version

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,10 +56,36 @@ intellijPlatform {
 
     pluginConfiguration {
         name = properties("pluginName").get()
+        version = properties("pluginVersion").get()
 
         ideaVersion {
             sinceBuild = properties("pluginSinceBuild")
             untilBuild = properties("pluginUntilBuild")
+        }
+
+        description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
+
+            with(it.lines()) {
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+            }
+        }
+
+        val changelog = project.changelog // local variable for configuration cache compatibility
+        // Get the latest available change notes from the changelog file
+        changeNotes = properties("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
         }
     }
 
@@ -84,7 +110,9 @@ intellijPlatform {
 
     pluginVerification {
         ides {
-            ide("IU-2025.2")
+            ide("IC-2025.2")
+            // test on available 2025.3 version
+            ide("IU-253.17525.95")
         }
     }
 }
@@ -128,38 +156,6 @@ tasks {
             listOf(
                 "-Didea.log.debug.categories=com.cycode.plugin"
             )
-        }
-    }
-
-    patchPluginXml {
-        version = properties("pluginVersion").get()
-        sinceBuild = properties("pluginSinceBuild").get()
-        untilBuild = properties("pluginUntilBuild").get()
-
-        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-            val start = "<!-- Plugin description -->"
-            val end = "<!-- Plugin description end -->"
-
-            with(it.lines()) {
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                }
-                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
-            }
-        }
-
-        val changelog = project.changelog // local variable for configuration cache compatibility
-        // Get the latest available change notes from the changelog file
-        changeNotes = properties("pluginVersion").map { pluginVersion ->
-            with(changelog) {
-                renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
-                        .withHeader(false)
-                        .withEmptySections(false),
-                    Changelog.OutputType.HTML,
-                )
-            }
         }
     }
 }
